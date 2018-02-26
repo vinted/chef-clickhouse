70bf7709d26640a0f153a22f5411a34f0ed7c524
Ernestas Poskus <ernestas.poskus@gmail.com>
2018-02-26 17:04:43 +0200

    Add initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-clickhouse
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef-clickhouse
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
